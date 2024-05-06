08ed99435765bcb801d7f7e18fff5c2a7e5e442f
Talha Riaz <muhammadtalha1005@gmail.com>
2024-05-06 13:42:07 +0500

    getallitems

--- api/fooditem/fooditem.js
+++ api/fooditem/fooditem.js
@@ -14,11 +14,26 @@ router.post('/addfooditem', async (req, res) => {
             itemPrice: itemPrice,
         })
         await item.save();
-        res.status(200).json({message:"Item add successfully"})
+        res.status(200).json({message:"Item add successfully",item})
     } catch (error) {
         res.status(500).json({message:"Internel server error"})
     }
 })
+
+router.get('/getallitems',async (req,res)=>{
+    try {
+        const data= await Food.find();
+        if(!data){
+            res.status(400).json({message:"items not found"});
+        }
+
+        res.status(200).json({data});
+    } catch (error) {
+        res.json({message:"Internel server error"})
+    }
+})
+
+
 
 
 module.exports = router;
